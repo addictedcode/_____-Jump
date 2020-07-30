2dea602e9e3901ec325885f97cd6945df84ecc17
Phiryon <villaluzpa@gmail.com>
2020-07-30 21:46:34 +0800

    Added Plane to Sample Scene
    Added Ball Prefab
    
    Added Spawning Script with complete events

---
+++ _____-Jump/Assets/Scripts/HO - Event Broadcasting/Spawning.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private GameObject spherePrefab;
+    private int numberOfBallsToSpawn = 1;
+
+    void Awake()
+    {
+        EventBroadcaster.Instance.AddObserver(
+            EventNames.HOEventBroadcasting.INPUT_NUMBER_BALLS_CHANGED,
+            this.UpdateBallNumber
+            );
+        EventBroadcaster.Instance.AddObserver(
+            EventNames.HOEventBroadcasting.BUTTON_SPAWN_BALLS_PRESSED,
+            this.SpawnBalls
+            );
+        EventBroadcaster.Instance.AddObserver(
+            EventNames.HOEventBroadcasting.BUTTON_CLEAR_ALL_PRESSED,
+            this.ClearBalls
+            );
+    }
+
+    private void OnDestroy()
+    {
+        EventBroadcaster.Instance.RemoveObserver(
+            EventNames.HOEventBroadcasting.INPUT_NUMBER_BALLS_CHANGED);
+        EventBroadcaster.Instance.RemoveObserver(
+            EventNames.HOEventBroadcasting.BUTTON_SPAWN_BALLS_PRESSED);
+        EventBroadcaster.Instance.RemoveActionAtObserver(
+            EventNames.HOEventBroadcasting.BUTTON_CLEAR_ALL_PRESSED,
+            this.ClearBalls);
+    }
+
+    private void UpdateBallNumber(Parameters param)
+    {
+        numberOfBallsToSpawn = param.GetIntExtra("ballCount", 0);
+    }
+    private void SpawnBalls()
+    {
+        for(int i = 0; i < numberOfBallsToSpawn; i++)
+        {
+            Instantiate(spherePrefab, Vector3.zero, Quaternion.identity, this.transform);
+        }
+    }
+    private void ClearBalls()
+    {
+        foreach (Transform child in this.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+
+
+}
