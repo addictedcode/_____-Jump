5ab4d91f060d8951bcdf75cdfd80ba1c9924519a
Addictedcode <richmond_w_ngo@dlsu.edu.ph>
2020-09-12 19:31:29 +0800

    Updated for trailer. added scene transition.

---
+++ _____-Jump/Assets/Scripts/Scenes/Loading.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    AsyncOperation loadingOperation;
+    Slider progressBar;
+
+    private void Start()
+    {
+        loadingOperation = SceneManager.LoadSceneAsync(1);
+    }
+
+    void Update()
+    {
+        progressBar.value = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+    }
+}
---
+++ _____-Jump/Assets/Scripts/Scenes/Winbox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winbox : MonoBehaviour
+{
+    [SerializeField] private int nextSceneIndex = 0;
+    [SerializeField] private GameObject loadingScreen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            loadingScreen.SetActive(true);
+        }
+    }
+}
